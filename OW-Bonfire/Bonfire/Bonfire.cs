020693beb8a6d3642f08c4f2db8c6b2f5c3c644c
Just1Developer <weisserbenjamin98@gmail.com>
2024-05-26 10:33:36 +0200

    Bonfire RunProcess (incomplete) - Transfer of files to other pc -> push

--- OW-Bonfire/Bonfire/Bonfire.cs
+++ OW-Bonfire/Bonfire/Bonfire.cs
@@ -6,11 +6,22 @@ public class Bonfire
 {
     private static int _overwatchId = -1;
     private static Dictionary<string, string> _arguments;
+    private static bool IS_WINDOWS = false;
+    private static string run_process_cmd;
+
+    private static string newName, oldName;
     
     public static async Task<int> Launch(string[] args)
     {
         _arguments = new();
         ParseArgs(args);
+
+        // Todo I don't think we need the suffix when executing via cmd
+        string suffix = IS_WINDOWS ? ".exe" : "";
+        newName = $"Overwatch_new{suffix}";
+        oldName = $"Overwatch{suffix}";
+        run_process_cmd = IS_WINDOWS ? run_process_win : run_process_unix;
+        
         // Wait for overwatch to shut down before renaming the new files and relaunching overwatch
         if (_overwatchId >= 0)
         {
@@ -28,6 +39,19 @@ public class Bonfire
             return 1;
         }
         return 0;
+    }
+    
+    internal static ProcessStartInfo GetCmdInfo(string workingDirectory, string run_process_cmd, string arguments)
+    {
+        return new ProcessStartInfo()
+        {
+            WorkingDirectory = workingDirectory,
+            FileName = run_process_cmd,
+            Arguments = arguments,
+            CreateNoWindow = true, // This prevents the command window from showing up
+            UseShellExecute = false, // Necessary to redirect input/output if needed
+            RedirectStandardOutput = true, // To capture the output if needed
+        };
     }
 
     private static void ParseArgs(string[] args)
@@ -52,6 +76,9 @@ public class Bonfire
         {
             case "processId":
                 _overwatchId = int.Parse(value);
+                break;
+            case "windows":
+                IS_WINDOWS = true;
                 break;
             default:
                 _arguments[arg] = value;
